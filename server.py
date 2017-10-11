a2947ee1961f1f2edf142b7ac230a4a35fd3f9b2
carmenvirginialopezgilperez <32704704+carmenvirginialopezgilperez@users.noreply.github.com>
2017-10-11 17:11:43 +0200

    Nuevo server

--- server.py
+++ server.py
@@ -1,7 +1,63 @@
 from flask import Flask
+import requests
+from flask import Response
+from flask import request
+import json
 app = Flask(__name__)
+
 @app.route('/')
 def index():
 	return 'Yo, its working!'
+
+#http://127.0.0.1:5000/vehiculo/?matricula=12
+@app.route('/vehiculo/', methods=["GET"])
+def vehiculo_preguntar():
+	args = request.args
+	text_un = args.get("matricula", None)
+	data = {}
+	data['matricula'] = text_un
+	json_data = json.dumps(data)
+	url = 'http://104.154.101.83/vehiculo/?matricula='
+	url_final = url + text_un
+	return requests.get(url_final).content
+	# Response(json_data, status=200, mimetype="application/json")
+
+
+@app.route('/alias/', methods=["GET"])
+def persona_preguntar():
+	args = request.args
+	text_un = args.get("dni", None)
+	data = {}
+	data['dni'] = text_un
+	json_data = json.dumps(data)
+	url = 'http://104.154.101.83/alias/?dni='
+	url_final = url + text_un
+	return requests.get(url_final).content
+
+@app.route('/auxilio/', methods=["GET"])
+def crear_auxilio():
+	args = request.args
+	text_gps = args.get("lugares", None)
+	text_fecha = args.get("fechaConocimiento", None)
+	text_persona = args.get("nombre", None)
+	text_un = {"properties": { "lugares": {text_gps}, "fechaConocimiento": text_fecha, "nombre", text_persona}}
+	json_data = json.dumps(text_un)
+	url = 'http://104.154.101.83/alias/?dni='
+	url_final = url + text_un
+	return requests.get(url_final).content
+
+@app.route('/avistamiento/', methods=["GET"])
+def crear_avistamiento():
+	args = request.args
+	text_gps = args.get("lugares", None)
+	text_fecha = args.get("fechaConocimiento", None)
+	text_persona = args.get("nombre", None)
+	text_un = {"properties": { "lugares": {text_gps}, "fechaConocimiento": text_fecha, "nombre", text_persona}}
+	json_data = json.dumps(text_un)
+	url = 'http://104.154.101.83/alias/?dni='
+	url_final = url + text_un
+	return requests.get(url_final).content
+
+
 if __name__ == "__main__":
 	app.run()
