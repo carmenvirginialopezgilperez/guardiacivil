d9d3a1db2ec8c38625e181cc3da167e862b8aad7
carmenvirginialopezgilperez <32704704+carmenvirginialopezgilperez@users.noreply.github.com>
2017-10-27 11:56:02 +0200

    Update server.py

--- server.py
+++ server.py
@@ -8,25 +8,21 @@ from flask_cors import cross_origin, CORS
 app = Flask(__name__)
 cors = CORS(app)
 
-@app.route('/', methods=["POST"])
-def index():
-	response_index = ""
-	text_total = request.json
-	text_result = text_total["result"]
-	text_pa = text_result["parameters"]
-	text_consulta = text_pa["consulta"]
-	if text_consulta == "1":
-		response_index = persona_preguntar()
-	elif text_consulta == "2":
-		print("consulta vehiculo")
-		response_index = vehiculo_preguntar()
-	elif text_consulta == "3":
-		response_index = crear_identificacion()
-	elif text_consulta == "4":
-		response_index = crear_avistamiento()
-	else:
-		response_index = crear_auxilio()
-	return response_index
+@app.route("/selenium/", methods=["GET"])
+@cross_origin("*")
+def selenium():
+	driver = webdriver.PhantomJS()
+	endpoint = "FashionWeek"
+	url = "https://twitter.com/"
+	url_final = url + endpoint
+	driver.get(url_final)
+	driver.implicitly_wait(10)
+	lista_tweets = driver.find_elements_by_tag_name('p')
+	tweets = ""
+	for tweet in lista_tweets:
+		tweets += tweet.text
+	print(tweets)
+	return tweets
 
 @app.route('/vehiculos', methods=["POST"])
 def vehiculo_preguntar():
@@ -57,104 +53,7 @@ def vehiculo_preguntar():
 	final_texto={"speech":final,"displayText":final,"data":{},"contextOut":[],"source":"webhook"}
 	json_data_final = json.dumps(final_texto)
 	print(json_data_final)
-	return Response(json_data_final, status=200, mimetype="application/json")
-
-
-@app.route('/alias', methods=["POST"])
-def persona_preguntar():
-	text_total = request.json
-	text_result = text_total["result"]
-	text_pa = text_result["parameters"]
-	text_un = text_pa["dni"]
-	text_un = text_un.replace(" ", "")
-	data = {}
-	data['dni'] = text_un
-	json_data = json.dumps(data)
-	url = 'http://104.154.101.83/alias/?dni='
-	url_final = url + text_un
-	respuesta = requests.get(url_final).content
-	s = requests.session()
-	s.keep_alive = False
-	my_json = respuesta.decode('utf8').replace("'", '"')
-	datajson = json.loads(my_json)
-	respuesta_pro = datajson["properties"]
-	respuesta_re = respuesta_pro["resenas"]
-	respuesta_item = respuesta_re["items"]
-	respuesta_img = respuesta_item["ref"]
-	final = "La url de la imagen con dni "+text_un+" es " +respuesta_img
-	final_texto={"speech":final,"displayText":final,"data":{},"contextOut":[],"source":"webhook"}
-	json_data_final = json.dumps(final_texto)
-	return Response(json_data_final, status=200, mimetype="application/json")
-
-@app.route('/auxilio', methods=["POST"])
-def crear_auxilio():
-	text_total = request.json
-	text_result = text_total["result"]
-	text_pa = text_result["parameters"]
-	text_un = text_pa["dni"]
-	text_un = text_un.replace(" ", "")
-	data = {}
-	data['dni'] = text_un
-	json_data = json.dumps(data)
-	url = 'http://104.154.101.83/registro/?id='
-	url_final = url + text_un
-	respuesta = requests.get(url_final).content
-	s = requests.session()
-	s.keep_alive = False
-	my_json = respuesta.decode('utf8').replace("'", '"')
-	datajson = json.loads(my_json)
-	respuesta_pro = datajson["respuesta"]
-	final = "Registrado auxilio"
-	final_texto={"speech":final,"displayText":final,"data":{},"contextOut":[],"source":"webhook"}
-	json_data_final = json.dumps(final_texto)
-	return Response(json_data_final, status=200, mimetype="application/json")
-
-@app.route('/identificacion', methods=["POST"])
-def crear_identificacion():
-	text_total = request.json
-	text_result = text_total["result"]
-	text_pa = text_result["parameters"]
-	text_un = text_pa["dni"]
-	text_un = text_un.replace(" ", "")
-	data = {}
-	data['dni'] = text_un
-	json_data = json.dumps(data)
-	url = 'http://104.154.101.83/registro/?id='
-	url_final = url + text_un
-	respuesta = requests.get(url_final).content
-	s = requests.session()
-	s.keep_alive = False
-	my_json = respuesta.decode('utf8').replace("'", '"')
-	datajson = json.loads(my_json)
-	respuesta_pro = datajson["respuesta"]
-	final = "Registrada identificacion"
-	final_texto={"speech":final,"displayText":final,"data":{},"contextOut":[],"source":"webhook"}
-	json_data_final = json.dumps(final_texto)
-	return Response(json_data_final, status=200, mimetype="application/json")
-
-@app.route('/avistamiento', methods=["POST"])
-def crear_avistamiento():
-	text_total = request.json
-	text_result = text_total["result"]
-	text_pa = text_result["parameters"]
-	text_un = text_pa["matricula"]
-	text_un = text_un.replace(" ", "")
-	data = {}
-	data['dni'] = text_un
-	json_data = json.dumps(data)
-	url = 'http://104.154.101.83/registro/?id='
-	url_final = url + text_un
-	respuesta = requests.get(url_final).content
-	s = requests.session()
-	s.keep_alive = False
-	my_json = respuesta.decode('utf8').replace("'", '"')
-	datajson = json.loads(my_json)
-	respuesta_pro = datajson["respuesta"]
-	final = "Registrado avistamiento"
-	final_texto={"speech":final,"displayText":final,"data":{},"contextOut":[],"source":"webhook"}
-	json_data_final = json.dumps(final_texto)
-	return Response(json_data_final, status=200, mimetype="application/json")
-
+	return Response(json_data_final, status=200, mimetype="application/json"
 
 if __name__ == "__main__":
 	app.run()
